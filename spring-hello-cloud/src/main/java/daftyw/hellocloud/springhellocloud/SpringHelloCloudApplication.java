fd598171bb0dd4a0d32d646543560351becb8162
Rawin Ngamloet <windx7@gmail.com>
2019-09-04 06:20:11 +0700

    init

---
+++ spring-hello-cloud/src/main/java/daftyw/hellocloud/springhellocloud/SpringHelloCloudApplication.java
@@ -0,0 +1,13 @@
+package daftyw.hellocloud.springhellocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHelloCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHelloCloudApplication.class, args);
+	}
+
+}
---
+++ spring-hello-cloud/src/test/java/daftyw/hellocloud/springhellocloud/SpringHelloCloudApplicationTests.java
@@ -0,0 +1,16 @@
+package daftyw.hellocloud.springhellocloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringHelloCloudApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
