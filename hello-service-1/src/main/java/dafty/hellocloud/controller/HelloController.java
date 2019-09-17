725c2cb0ea2615c4cad36795afea520114826d1d
Rawin Ngamloet <windx7@gmail.com>
2019-09-18 03:30:40 +0700

    Add more change

--- hello-service-1/src/main/java/dafty/hellocloud/controller/HelloController.java
+++ hello-service-1/src/main/java/dafty/hellocloud/controller/HelloController.java
@@ -1,5 +1,9 @@
 package dafty.hellocloud.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,11 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+	private static
+	Logger log = LoggerFactory.getLogger(HelloController.class); 
+	
 	@Value("${service1.value1}")
 	private String serviceValue;
 	
     @GetMapping("/hello")
-    public ResponseEntity<String> hello() {
+    public ResponseEntity<String> hello(HttpServletRequest request) {
+    	log.debug("X-Request-Origin={}", request.getHeader("X-Request-Origin"));
         return ResponseEntity.ok().body(serviceValue);
     }
 }
